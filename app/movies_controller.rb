4f6a2bf9384297f2a963c0d9d95d7ce336179ee4
Matthew Jones <matts3371@gmail.com>
2020-02-08 03:00:36 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,8 +29,8 @@ def can_be_created_in_a_block(args = {:title => "Home Alone", :release_date => "
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = "woo"
-    m.release_date = "yay"
+    m.title = args[0]
+    m.release_date = args[1]
   end
 end
 
